a18736c032ca499398fd30df57c1c20fd69e417f
Cəmil M. Camalov <camil.camalov@cybernet.az>
2025-07-15 10:18:52 +0400

    init

---
+++ user-management/src/main/java/az/cybernet/usermanagement/UserManagement.java
@@ -0,0 +1,12 @@
+package az.cybernet.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagement {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagement.class, args);
+    }
+}
