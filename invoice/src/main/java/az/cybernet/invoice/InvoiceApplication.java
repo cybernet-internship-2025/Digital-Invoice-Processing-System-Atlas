2132861336c474b8f885f641fea62396c3b66858
Cəmil M. Camalov <camil.camalov@cybernet.az>
2025-07-17 03:00:21 +0400

    allow corss origins - app based

--- invoice/src/main/java/az/cybernet/invoice/InvoiceApplication.java
+++ invoice/src/main/java/az/cybernet/invoice/InvoiceApplication.java
@@ -2,8 +2,10 @@ package az.cybernet.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class InvoiceApplication {
 
     public static void main(String[] args) {
