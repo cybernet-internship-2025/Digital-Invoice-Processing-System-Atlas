debf19d3eef9aee95859c38d676c5580faa9472b
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-07-24 21:44:32 +0400

    Moved previously written mapstruct code to InvoiceMapstruct. Added createInvoice to InvoiceService

--- invoice/src/main/java/az/cybernet/invoice/mapstruct/InvoiceMapstruct.java
+++ invoice/src/main/java/az/cybernet/invoice/mapstruct/InvoiceMapstruct.java
@@ -1,9 +1,18 @@
 package az.cybernet.invoice.mapstruct;
 
+import az.cybernet.invoice.dto.request.CreateInvoiceRequest;
+import az.cybernet.invoice.dto.request.InvoiceProductRequest;
 import az.cybernet.invoice.dto.request.InvoiceRequest;
+import az.cybernet.invoice.dto.request.ProductQuantityRequest;
 import az.cybernet.invoice.dto.response.InvoiceResponse;
 import az.cybernet.invoice.entity.Invoice;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface InvoiceMapstruct {
@@ -11,4 +20,21 @@ public interface InvoiceMapstruct {
     Invoice toEntity(InvoiceRequest InvoiceRequest);
 
     InvoiceResponse toDto(Invoice invoice);
+
+    InvoiceRequest getInvoiceFromCreateRequest(CreateInvoiceRequest request);
+
+    @Mapping(target = "invoiceId", source = "id")
+    @Mapping(target = "productId", source = "product.id")
+    @Mapping(target = "quantity", source = "product.quantity")
+    InvoiceProductRequest toInvoiceProductRequest(@MappingTarget UUID invoiceId, ProductQuantityRequest product);
+
+    default List<InvoiceProductRequest> toInvoiceProductRequestList(CreateInvoiceRequest request) {
+        if (request == null || request.getProductQuantityRequests() == null) {
+            return List.of();
+        }
+        return request.getProductQuantityRequests()
+                .stream()
+                .map(product -> toInvoiceProductRequest(request.getId(), product))
+                .collect(Collectors.toList());
+    }
 }
--- invoice/src/main/java/az/cybernet/invoice/service/impl/InvoiceServiceImpl.java
+++ invoice/src/main/java/az/cybernet/invoice/service/impl/InvoiceServiceImpl.java
@@ -1,14 +1,18 @@
 package az.cybernet.invoice.service.impl;
 
 import az.cybernet.invoice.dto.request.CreateInvoiceRequest;
+import az.cybernet.invoice.dto.request.InvoiceProductRequest;
 import az.cybernet.invoice.dto.response.InvoiceResponse;
 import az.cybernet.invoice.entity.Invoice;
 import az.cybernet.invoice.mapper.InvoiceMapper;
 import az.cybernet.invoice.mapstruct.InvoiceMapstruct;
+import az.cybernet.invoice.service.InvoiceProductService;
 import az.cybernet.invoice.service.InvoiceService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
@@ -16,18 +20,28 @@ public class InvoiceServiceImpl implements InvoiceService {
     private final InvoiceMapper mapper;
     private final InvoiceMapstruct mapstruct;
 
-    public InvoiceServiceImpl(InvoiceMapper mapper, InvoiceMapstruct mapstruct) {
+    private final InvoiceProductService invoiceProductService;
+
+    public InvoiceServiceImpl(InvoiceMapper mapper,
+                              InvoiceMapstruct mapstruct,
+                              InvoiceProductService invoiceProductService) {
         this.mapper = mapper;
         this.mapstruct = mapstruct;
+        this.invoiceProductService = invoiceProductService;
     }
 
     @Override
+    @Transactional
     public InvoiceResponse createInvoice(CreateInvoiceRequest request) {
-        Invoice invoice = mapstruct.toEntity(request);
+        Invoice invoice = mapstruct.toEntity(
+                mapstruct.getInvoiceFromCreateRequest(request));
         invoice.setCreatedAt(LocalDateTime.now());
         invoice.setUpdatedAt(LocalDateTime.now());
 
+        List<InvoiceProductRequest> invoiceProductList = mapstruct.toInvoiceProductRequestList(request);
+
         mapper.insertInvoice(invoice);
+        invoiceProductList.forEach(invoiceProductService::insertInvoiceProduct);
         return mapstruct.toDto(invoice);
     }
 }
