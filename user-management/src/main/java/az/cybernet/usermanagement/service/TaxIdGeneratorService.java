11b81c54cad73e3290ead2bfb32cdf328fe8872c
balash300 <balashuseynov300@gmail.com>
2025-07-29 07:34:49 +0400

    fix: application.yml fixed

--- user-management/src/main/java/az/cybernet/usermanagement/service/TaxIdGeneratorService.java
+++
@@ -1,6 +0,0 @@
-package az.cybernet.usermanagement.service;
-
-public interface TaxIdGeneratorService {
-
-    String generateUniqueTaxId();
-}
--- user-management/src/main/java/az/cybernet/usermanagement/util/handler/UUIDTypeHandler.java
+++ user-management/src/main/java/az/cybernet/usermanagement/util/handler/UUIDTypeHandler.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 @Slf4j
 public class UUIDTypeHandler implements TypeHandler<UUID> {
 
+    @Override
     public void setParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
         if (parameter == null) {
             ps.setObject(i, (Object)null, 1111);
@@ -22,14 +23,17 @@ public class UUIDTypeHandler implements TypeHandler<UUID> {
 
     }
 
+    @Override
     public UUID getResult(ResultSet rs, String columnName) throws SQLException {
         return toUUID(rs.getString(columnName));
     }
 
+    @Override
     public UUID getResult(ResultSet rs, int columnIndex) throws SQLException {
         return toUUID(rs.getString(columnIndex));
     }
 
+    @Override
     public UUID getResult(CallableStatement cs, int columnIndex) throws SQLException {
         return toUUID(cs.getString(columnIndex));
     }
