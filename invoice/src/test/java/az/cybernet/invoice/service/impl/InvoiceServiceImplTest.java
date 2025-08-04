780b3eb3ffe0f946838b76bcc00b04c461389ebc
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-08-05 00:49:11 +0400

    Added createInvoice_SouldCorrectyCreateInvoice test

--- invoice/src/test/java/az/cybernet/invoice/service/impl/InvoiceServiceImplTest.java
+++ invoice/src/test/java/az/cybernet/invoice/service/impl/InvoiceServiceImplTest.java
@@ -1,13 +1,20 @@
 package az.cybernet.invoice.service.impl;
 
-import az.cybernet.invoice.dto.request.InvoiceCorrectionReq;
+import az.cybernet.invoice.dto.request.*;
+import az.cybernet.invoice.dto.response.InvoiceProductResponse;
 import az.cybernet.invoice.dto.response.InvoiceResponse;
+import az.cybernet.invoice.dto.response.ProductResponse;
 import az.cybernet.invoice.entity.Invoice;
 import az.cybernet.invoice.entity.InvoiceOperation;
+import az.cybernet.invoice.enums.Status;
 import az.cybernet.invoice.exceptions.InvoiceNotFoundException;
 import az.cybernet.invoice.mapper.InvoiceMapper;
 import az.cybernet.invoice.mapper.InvoiceOperationMapper;
 import az.cybernet.invoice.mapstruct.InvoiceMapstruct;
+import az.cybernet.invoice.mapstruct.InvoiceProductMapstruct;
+import az.cybernet.invoice.mapstruct.ProductMapstruct;
+import az.cybernet.invoice.service.InvoiceProductService;
+import az.cybernet.invoice.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,6 +22,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static az.cybernet.invoice.constant.Constants.INVD;
@@ -32,6 +41,18 @@ class InvoiceServiceImplTest {
 
     @Mock
     private InvoiceOperationMapper invoiceOperationMapper;
+
+    @Mock
+    private InvoiceProductMapstruct invoiceProductMapstruct;
+
+    @Mock
+    private ProductMapstruct productMapstruct;
+
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private InvoiceProductService invoiceProductService;
 
     @InjectMocks
     private InvoiceServiceImpl service;
@@ -90,11 +111,76 @@ class InvoiceServiceImplTest {
         LocalDate now = LocalDate.now();
         String expectedPrefix = INVD + String.format("%02d", now.getYear() % 100) + String.format("%02d"
                 , now.getMonthValue());
-        when(mapper.getLastInvoiceNumberOfMonth(any(), any())).thenReturn(12);
+        when(mapper.getLastInvoiceNumberOfMonth(any(), any())).thenReturn(null);
 
         String invoiceNumber = service.generateInvoiceNumber();
 
         assertTrue(invoiceNumber.startsWith(expectedPrefix));
         assertTrue(invoiceNumber.endsWith("0013"));
+    }
+
+    @Test
+    void createInvoice_ShouldCorrectlyCreateInvoice() {
+        CreateInvoiceRequest request = new CreateInvoiceRequest();
+
+        LocalDate now = LocalDate.now();
+        int expectedNumber = Integer.parseInt(String.format("%02d", now.getYear() % 100) + String.format("%02d", now.getMonthValue()))
+                * 10000 + 1;
+
+        ProductQuantityRequest product1 = new ProductQuantityRequest();
+        product1.setQuantity(2.0);
+        product1.setPrice(100.0);
+
+        ProductQuantityRequest product2 = new ProductQuantityRequest();
+        product2.setQuantity(1.0);
+        product2.setPrice(150.0);
+
+        List<ProductQuantityRequest> productQuantityList = List.of(product1, product2);
+        request.setProductQuantityRequests(productQuantityList);
+
+        InvoiceRequest expectedRequest = new InvoiceRequest();
+        when(mapstruct.getInvoiceFromCreateRequest(eq(request))).thenReturn(expectedRequest);
+        when(mapstruct.toEntity(expectedRequest)).thenReturn(new Invoice());
+
+        when(mapper.getLastInvoiceNumberOfMonth(any(), any())).thenReturn(null);
+
+        List<InvoiceProductRequest> invoiceProductRequestList = List.of(
+                new InvoiceProductRequest(), new InvoiceProductRequest());
+        when(invoiceProductMapstruct.toInvoiceProductRequestList(any(UUID.class), eq(productQuantityList))).thenReturn(invoiceProductRequestList);
+
+        List<ProductRequest> productRequestList = List.of(
+                new ProductRequest(), new ProductRequest());
+        when(productMapstruct.toProductRequestList(eq(productQuantityList))).thenReturn(productRequestList);
+
+        doNothing().when(mapper).insertInvoice(any(Invoice.class));
+        when(productService.insertProduct(any(ProductRequest.class))).thenReturn(new ProductResponse());
+        when(invoiceProductService.insertInvoiceProduct(any(InvoiceProductRequest.class))).thenReturn(new InvoiceProductResponse());
+
+        when(mapstruct.toDto(any(Invoice.class))).thenAnswer(invocation -> {
+            Invoice invoiceArg = invocation.getArgument(0);
+
+            InvoiceResponse response = new InvoiceResponse();
+            response.setId(invoiceArg.getId());
+            response.setSeries(invoiceArg.getSeries());
+            response.setInvoiceNumber(invoiceArg.getInvoiceNumber());
+            response.setStatus(invoiceArg.getStatus());
+            response.setTotal(invoiceArg.getTotal());
+
+            return response;
+        });
+
+        InvoiceResponse response = service.createInvoice(request);
+
+        assertNotNull(response);
+
+        assertNotNull(response.getId());
+        assertEquals(350.0, response.getTotal());
+        assertEquals(Status.PENDING, response.getStatus());
+        assertEquals("INVD", response.getSeries());
+        assertEquals(expectedNumber, response.getInvoiceNumber());
+
+        verify(mapper).insertInvoice(any(Invoice.class));
+        verify(productService, times(2)).insertProduct(any(ProductRequest.class));
+        verify(invoiceProductService, times(2)).insertInvoiceProduct(any(InvoiceProductRequest.class));
     }
 }
