96d2b79f4b3259362da0a076bac30be4c425f2d8
Elvin-git-2025 <agazadeelvin086@gmail.com>
2025-08-09 23:46:15 +0400

    new changes

---
+++ invoice/src/main/java/az/cybernet/invoice/entity/ReturnTypeInvoice.java
@@ -0,0 +1,31 @@
+package az.cybernet.invoice.entity;
+import az.cybernet.invoice.dto.response.ProductDetailResponse;
+import az.cybernet.invoice.enums.Status;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class ReturnTypeInvoice {
+    UUID id;
+    String series;
+    Integer invoiceNumber;
+    UUID senderId;
+    UUID customerId;
+    Status status;
+    Double total;
+    LocalDateTime updatedAt;
+    String comment;
+    List<ProductDetailResponse> products;
+
+    UUID originalInvoiceId; // Reference to the original invoice being returned
+    String originalInvoiceSeries; // Series of the original invoice
+    LocalDateTime returnDate; // Date of the return operation
+}
