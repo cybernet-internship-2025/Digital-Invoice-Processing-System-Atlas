3132614cc87601a100cecc151f3a15ff9d5f7975
Tamilla Huseynova <ta.huseynova@gmail.com>
2025-08-14 12:33:28 +0400

    config file created, application file fix

--- integration/src/main/java/az/cybernet/integration/controller/IAMASController.java
+++ integration/src/main/java/az/cybernet/integration/controller/IAMASController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/iamas")
+@RequestMapping("/v1/iamas")
 public class IAMASController {
 
     private final IAMASService iamasService;
