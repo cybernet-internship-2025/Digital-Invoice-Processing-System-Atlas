cfe88be4ec221ffecbfda0e5501fd50137a404fa
Fuadorujlu <fuadoruclu7@gmail.com>
2025-08-19 22:07:47 +0300

    Created the service layer, created the table named registrations, wrote insert methods in mapper and updated the Controller class for Post method accordingly. Also gave missing fields to Response Entities, and Registration entity.

--- user-management/src/main/java/az/cybernet/usermanagement/dto/response/LegalEntityRegistrationResponse.java
+++ user-management/src/main/java/az/cybernet/usermanagement/dto/response/LegalEntityRegistrationResponse.java
@@ -1,4 +1,11 @@
 package az.cybernet.usermanagement.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LegalEntityRegistrationResponse {
+    String legalEntityName;
+    String phoneNumber;
 }
