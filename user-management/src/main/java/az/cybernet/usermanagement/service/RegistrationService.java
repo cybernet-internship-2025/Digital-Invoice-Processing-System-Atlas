ee682a689a19a02d1b8a21da291bc8db7f627dea
agharahimov <agarahimovagasi2@gmail.com>
2025-08-21 10:58:58 +0400

    added kafka-broker and zookeeper to docker compose

--- user-management/src/main/java/az/cybernet/usermanagement/service/RegistrationService.java
+++
@@ -1,4 +0,0 @@
-package az.cybernet.usermanagement.service;
-
-public interface RegistrationService {
-}
--- user-management/src/main/java/az/cybernet/usermanagement/service/impl/RegistrationServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package az.cybernet.usermanagement.service.impl;
-
-import az.cybernet.usermanagement.service.RegistrationService;
-
-public class RegistrationServiceImpl implements RegistrationService {
-}
