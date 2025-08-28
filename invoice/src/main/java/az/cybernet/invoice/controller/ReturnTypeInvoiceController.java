5869e0316b3a3025b8d0917ebda7c2dc30565e57
GoychakAhmadova <goychak.ahmadova77@gmail.com>
2025-08-28 12:38:55 +0400

    Correction of injection

--- invoice/src/main/java/az/cybernet/invoice/controller/ReturnTypeInvoiceController.java
+++ invoice/src/main/java/az/cybernet/invoice/controller/ReturnTypeInvoiceController.java
@@ -4,16 +4,17 @@ package az.cybernet.invoice.controller;
 import az.cybernet.invoice.dto.request.CreateReturnTypeRequest;
 import az.cybernet.invoice.entity.ReturnTypeInvoice;
 import az.cybernet.invoice.service.ReturnTypeInvoiceService;
-import az.cybernet.invoice.service.impl.ReturnTypeInvoiceServiceImpl;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/invoices/returns")
+@RequiredArgsConstructor
 public class ReturnTypeInvoiceController {
-    ReturnTypeInvoiceService returnTypeInvoiceService;
+    private final ReturnTypeInvoiceService returnTypeInvoiceService;
     @PostMapping
     public ResponseEntity<ReturnTypeInvoice> createReturnTypeInvoice(@RequestBody @Valid CreateReturnTypeRequest returnTypeInvoice) {
         return new ResponseEntity<>(returnTypeInvoiceService.createReturnTypeInvoice(returnTypeInvoice), HttpStatus.CREATED);
