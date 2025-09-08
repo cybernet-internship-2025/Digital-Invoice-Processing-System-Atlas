62844f86eecf291662ef7632717f1e1fdbeddb8c
balash300 <balashuseynov300@gmail.com>
2025-09-08 19:26:05 +0400

    feat: SecurityConfig in user-management is updated

--- user-management/src/main/java/az/cybernet/usermanagement/config/SecurityConfig.java
+++ user-management/src/main/java/az/cybernet/usermanagement/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package az.cybernet.usermanagement.config;
 
+import az.cybernet.authstarter.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
